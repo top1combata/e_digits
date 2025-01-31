f03bfffd985792a649d32f500454352b1493a5c7
///////// <savvamihalich@gmail.com>
2025-01-31 11:32:01 +0300

    fix header dependencies

--- functions.h
+++ functions.h
@@ -1,10 +1,10 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <time.h>
+#include <stdio.h>
 #include <gmp.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 void write_digits(mpz_t, mpz_t, uint64_t);
 void evaluate_p_q(mpz_t, mpz_t, uint64_t, uint64_t);
